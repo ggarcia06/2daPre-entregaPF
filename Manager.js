abdc47a6f6e045b8f9eb4bfd32f91aa41ec90d5b
German Garcia <ggarcia@g2tecnologia.com.ar>
2024-02-22 18:57:14 -0300

    se agrega una clase para el manejo de productos y carritos de compra

---
+++ Manager.js
@@ -0,0 +1,123 @@
+import fs from 'fs';
+import express from "express";
+
+export default class ObjManager {
+    array = new Array();
+
+    constructor (path){
+        this.path = path
+        if(!fs.existsSync(this.path)){
+            fs.writeFileSync(this.path, JSON.stringify(this.array))
+        }
+    }
+
+
+    getObjects = async() => {
+        const data = await fs.promises.readFile(this.path, 'utf-8')
+        const objects = JSON.parse(data)
+        return objects
+    }
+
+    //la funcion codeValidator permite saber si el codigo ingresado de un nuevo producto ya esta registrado en la base de datos
+    codeValidator = async(codeToValidate) => {
+        const data = await this.getObjects()
+        let result = data.find((el) => el.code === codeToValidate)
+        let isValid = false;
+        if(!result) {isValid = true}
+        return isValid;
+    }
+
+    addObject = async(object) => {
+
+        const validCode = await this.codeValidator(object.code)
+        if(!validCode){
+            console.log("el codigo ingresad ya esta registrado")
+        }else{
+            const objects = await this.getObjects()
+            object.status = true
+            object.id = 0
+            objects.length === 0 ? (object.id = 1) : (object.id = objects[objects.length - 1].id + 1) 
+            objects.push(object)
+
+            await fs.promises.writeFile(this.path, JSON.stringify(objects,  null, "\t"))
+            //return res.status(200).send({ status: "OK" })
+        }
+        return object
+    }
+
+    getObjectsById = async(requestedId) => {
+        const objects = await this.getObjects()
+        let result = objects.find((el) => el.id === requestedId)
+        if(!result){console.log("El id solicitado no existe")}
+        return result
+
+    }
+
+    deleteObject = async(requestedId ) => {
+        const objects = await this.getObjects()
+        let result = objects.find((el => el.id === requestedId))
+        let i = objects.indexOf(result)
+        objects.splice(i,1)
+
+        await fs.promises.writeFile(this.path, JSON.stringify(objects,  null, "\t"))
+     
+        return console.log(`el objeto con el id ${requestedId} se ha sido eliminado correctamente`)
+
+    }
+
+    updateObject = async(requestedId, updatedObject) => {
+        const objects = await this.getObjects()
+        let index = objects.findIndex((el) => el.id === requestedId)
+        objects[index] = updatedObject
+        objects[index].id = requestedId
+        await fs.promises.writeFile(this.path, JSON.stringify(objects,  null, "\t"))
+        return objects
+        
+    }
+}
+
+// const products = new ProductManager("./productos.json")
+
+
+
+// const productsbd = await products.getProducts()
+// console.log("TCL: productsbd", productsbd)
+
+
+
+// let producto = {
+//     title: "cebolla",
+//     description: "cebolla",
+//     price: 2500,
+//     thumbnail: "http://....",
+//     code: 99933,
+//     stock: 36
+// }
+// await products.addProduct(producto)
+
+
+
+// let newData = {
+//     title: "kiwi",
+//     description: "kiwi",
+//     price: 2500,
+//     thumbnail: "http://....",
+//     code: 36478,
+//     stock: 20
+// }
+// await products.updateProduct(1, newData)
+
+
+
+
+// await products.deleteProduct(1)
+
+
+
+// const result = await products.getProductsById(6)
+// console.log(result)
+
+
+
+
+    
--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 import express from "express";
 import fs, { write } from "fs";
 import UUID from "node:crypto"
+import Manager from "./Manager.js";
+import ObjManager from "./Manager.js";
 
 const app = express()
 const port = 8080
 const pathCart = "./data/carrito.json"
 const pathProducts = "./data/productos.json"
+const products = new ObjManager("./productos.json")
 
 app.listen(port, () => console.log("Servidor corriendo en puerto " + port))
 
@@ -15,43 +18,45 @@ app.use(express.json())
 //----------------------------------------Productos------------------------------
 app.get("/api/products/", async(req,res) => {
 
-//GET todos los productos
-const data = await fs.readFileSync(pathProducts, "utf-8")
-const products = JSON.parse(data)
-let limit = parseInt(req.query.limit) || products.length
-const finalProducts = products.slice(0,limit)
-res.send (finalProducts)
+    const productsdb = await products.getObjects()
+    let limit = parseInt(req.query.limit) || productsdb.length
+    const finalProducts = productsdb.slice(0,limit)
+    res.send (finalProducts)
 
 })
 
 app.get("/api/products/:pid/", async(req, res) => {
 
-    //GET el producto segun su id
-    const data = await fs.readFileSync(pathProducts, "utf-8")
-    const products = JSON.parse(data)
     let pid = parseInt(req.params.pid)
-    let result = products.find((el) => el.id === pid)
-    if(!result){res.send("El id solicitado no existe")}
+    const result = await products.getObjectsById(pid)
     res.send(result)
+
 })
 
 app.post("/api/products/", async(req, res) => {
 
-    //POST crear nuevo producto
-    const data = await fs.readFileSync(pathProducts, "utf-8")
-    const products = JSON.parse(data)
     let product = req.body
-
-    product.status = true
-    product.id = 0
-    products.length === 0 ? (product.id = 1) : (product.id = products[products.length - 1].id + 1)
     if (!product.title || !product.description  || !product.price || !product.code || !product.stock || !product.category) {
         return res.status(400).send({ status: "error" });
       }
     
-      products.push(product);
-      await fs.writeFileSync(pathProducts, JSON.stringify(products,  null, "\t"))
-      return res.status(200).send("Producto agregado correctamente")
+    await products.addObject(product)
+
+    // //POST crear nuevo producto
+    // const data = await fs.readFileSync(pathProducts, "utf-8")
+    // const products = JSON.parse(data)
+    // let product = req.body
+
+    // product.status = true
+    // product.id = 0
+    // products.length === 0 ? (product.id = 1) : (product.id = products[products.length - 1].id + 1)
+    // if (!product.title || !product.description  || !product.price || !product.code || !product.stock || !product.category) {
+    //     return res.status(400).send({ status: "error" });
+    //   }
+    
+    //   products.push(product);
+    //   await fs.writeFileSync(pathProducts, JSON.stringify(products,  null, "\t"))
+    return res.status(200).send({ status: "OK" })
     
 
 })
