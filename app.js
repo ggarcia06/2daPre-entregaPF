bac6813824e8947313212ea85f34985c0f87e5c8
German Garcia <ggarcia@g2tecnologia.com.ar>
2024-02-25 17:39:35 -0300

    se agregan rutas: para crear un carrito en carrito.json y para ver un carrito segun su id

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 import express from "express";
 import fs, { write, writeFileSync } from "fs";
-import UUID from "node:crypto";
 import Manager from "./Manager.js";
 import ObjManager from "./Manager.js";
 
@@ -9,7 +8,7 @@ const port = 8080
 const pathCart = "./data/carrito.json"
 const pathProducts = "./data/productos.json"
 const products = new ObjManager(pathProducts)
-//const carts = new ObjManager(pathCart)
+const carts = new ObjManager(pathCart)
 
 app.listen(port, () => console.log("Servidor corriendo en puerto " + port))
 
@@ -80,27 +79,36 @@ app.delete("/api/products/:pid/", async(req, res) => {
 
 app.post("/api/carts/", async(req, res) => {
 
-//     let carrito = await fs.readFileSync(pathCart, 'utf-8')
-//     let parsedCart = JSON.parse(carrito)
+    let carrito = await fs.promises.readFile(pathCart, 'utf-8')
+    let parsedCart = JSON.parse(carrito)
 
-//     const ID = randomUUID()
+    let cart = {
+        products: []
+    }
+    
+    cart.id = 0
+    parsedCart.length === 0 ? (cart.id = 1) : (cart.id = parsedCart[parsedCart.length - 1].id + 1)
+    console.log(cart)  
+     parsedCart.push(cart)
+     console.log(parsedCart)
+    await fs.promises.writeFile(pathCart, JSON.stringify(parsedCart, null, "\t"))
 
-//     let cart = {
-//         id: ID,
-//         products: []
-//     }
-
-//     carrito.push(cart)
-//     let finalData = JSON.stringify(carrito)
-//     fs.promises.writeFileSync(finalData, pathCart, null, "\t")
-
-//    return res.status(200).send({ status: "OK" })
+   return res.status(200).send({ status: "OK" })
 
 })
 
 app.get("/api/carts/:cid/", async(req, res) => {
 
+    let cid = parseInt(req.params.cid)
 
+    let cartById = await carts.getObjectsById(cid)
+    console.log("TCL: cartById", cartById)
+
+    if (!cartById) {
+        return res.status(400).send({ status: "error" });
+    }
+    
+   res.send(cartById)
 
     //GET mostrar productos que pertenezcan al carrito del id seleccionado
 
